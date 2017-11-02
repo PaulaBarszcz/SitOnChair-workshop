3875a7ec55744b571fdeb3bc677b104a2cd3dfe7
Paula Barszcz <paula.anna.barszcz@gmail.com>
2017-11-02 13:22:47 +0100

    Divided _base.scss to partial files for different sections + wrapped js into separate functions.

--- scripts/script.js
+++ scripts/script.js
@@ -2,89 +2,98 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // header-menu
 
-    var mobile = window.matchMedia("screen and (max-width: 536px)");
-    var trigger = document.getElementById('burger');
-    var nav = document.querySelector('nav');
+    var manageMenu = function () {
+        var mobile = window.matchMedia("screen and (max-width: 536px)");
+        var trigger = document.getElementById('burger');
+        var nav = document.querySelector('nav');
 
-    trigger.addEventListener('click',function(){
+        trigger.addEventListener('click',function(){
 
-        var display = nav.style.display;
-        if(display === 'block'){
-            nav.style.display = 'none';
-            nav.classList.remove("active-header-nav");
+            var display = nav.style.display;
+            if(display === 'block'){
+                nav.style.display = 'none';
+                nav.classList.remove("active-header-nav");
 
-        } else {
-            nav.style.display = 'block';
-            nav.classList.add("active-header-nav");
-        }
-    });
+            } else {
+                nav.style.display = 'block';
+                nav.classList.add("active-header-nav");
+            }
+        });
 
-    mobile.addListener( function(mob){
-        if (!mob.matches) {
-            nav.removeAttribute('style');
-            nav.classList.remove("active-header-nav");
-        }
-    });
+        mobile.addListener( function(mob){
+            if (!mob.matches) {
+                nav.removeAttribute('style');
+                nav.classList.remove("active-header-nav");
+            }
+        });
+    } //end of manageMenu definition
+
+    manageMenu();
 
 // slider
 
     //"The :scope pseudo-class restores the expected behavior, only matching selectors on descendants of the baseElement." - developer.mozilla.org
 
-    var slider = document.querySelector('#main-slider');
-    var prev = slider.querySelector(':scope .left');
-    var next = slider.querySelector(':scope .right');
-    var slides = slider.querySelectorAll(':scope .slide');
-    var currentSlide = 0;
-    var timer = null;
-    var timeDelay = 5000;
 
-    var timeoutNextSlide = function() {
-        timer = setTimeout(function() {
-            next.click();
-        }, timeDelay);
-    };
+    var manageSlider = function () {
 
-    var prevSlide = function() {
-       
-        for (var i=0; i<slides.length; i++) {
-            slides[i].classList.remove('active');
-        }
-        currentSlide--;
+        var slider = document.querySelector('#main-slider');
+        var prev = slider.querySelector(':scope .left');
+        var next = slider.querySelector(':scope .right');
+        var slides = slider.querySelectorAll(':scope .slide');
+        var currentSlide = 0;
+        var timer = null;
+        var timeDelay = 5000;
 
-        if (currentSlide < 0) {
-            currentSlide = slides.length-1;
-        }
+        var timeoutNextSlide = function() {
+            timer = setTimeout(function() {
+                next.click();
+            }, timeDelay);
+        };
 
-        slides[currentSlide].classList.add('active');
+        var prevSlide = function() {
+           
+            for (var i=0; i<slides.length; i++) {
+                slides[i].classList.remove('active');
+            }
+            currentSlide--;
 
-        clearTimeout(timer);
+            if (currentSlide < 0) {
+                currentSlide = slides.length-1;
+            }
+
+            slides[currentSlide].classList.add('active');
+
+            clearTimeout(timer);
+            timeoutNextSlide();
+        };
+
+        var nextSlide = function() {
+            
+            for (var i=0; i<slides.length; i++) {
+                slides[i].classList.remove('active');
+            }
+
+            currentSlide++;
+
+            if (currentSlide > slides.length-1) {
+                currentSlide = 0;
+            }
+            
+            slides[currentSlide].classList.add('active');
+
+            clearTimeout(timer);
+            timeoutNextSlide();
+        };
+
         timeoutNextSlide();
-    };
 
-    var nextSlide = function() {
-        
-        for (var i=0; i<slides.length; i++) {
-            slides[i].classList.remove('active');
-        }
+        prev.addEventListener('click', prevSlide);
+        next.addEventListener('click', nextSlide);
 
-        currentSlide++;
+    } //end of manageSlider definition
 
-        if (currentSlide > slides.length-1) {
-            currentSlide = 0;
-        }
-        
-        slides[currentSlide].classList.add('active');
-
-        clearTimeout(timer);
-        timeoutNextSlide();
-    };
-
-    timeoutNextSlide();
-
-    prev.addEventListener('click', prevSlide);
-    next.addEventListener('click', nextSlide);
-
-
+    manageSlider();
 
 
 
